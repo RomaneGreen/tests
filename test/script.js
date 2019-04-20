a06ed1b8328577e60d437a0ef820542d4d19f4d6
Romane G <romane71193@gmail.com>
2019-04-20 13:39:19 -0400

    added mock database

--- test/script.js
+++ test/script.js
@@ -17,4 +17,6 @@ const googleSearch = (searchInput) => {
     return matches.length > 3 ? matches.slice(0,3) : matches 
 }
 
-console.log(googleSearch('cats'))
+
+
+module.exports = googleSearch;
---
+++ test/script.test.js
@@ -0,0 +1,14 @@
+const googleSearch = require('./script');
+
+
+
+dbMock = [
+    'dog.com',
+    'cheespuff.com',
+    'disney.com',
+    'dogpics.com'
+]
+it('this is a test', () => {
+        googleSearch('testtest')
+})
+
