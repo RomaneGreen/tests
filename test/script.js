5b432319fe9b8954f13f3f6061864e0e0a7c5d77
Romane G <romane71193@gmail.com>
2019-04-20 13:22:42 -0400

    match function

--- test/script.js
+++ test/script.js
@@ -4,5 +4,14 @@ const googleDatabase = [
     'souprecipes.com',
     'flower.com',
     'animals.com',
-    
+    'catpics.com',
+    'myfavcats.com'
 ]
+
+const googleSearch = (searchInput) => {
+
+    const matches = googleDatabase.filter(website => {
+        return website.includes('searchInput')
+    })
+    return matches.length > 3 ? matches.slice(0,3) : matches 
+}
