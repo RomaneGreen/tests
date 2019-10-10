bdeecfbdedffb8ebee64f89c89e129a2454372dd
Romane G <romane71193@gmail.com>
2019-10-10 00:47:03 -0400

    header

--- gplaces/client/src/App.js
+++ gplaces/client/src/App.js
@@ -2,23 +2,22 @@ import React from 'react';
 import './App.css';
 import Places from './components/places'
 import QuestionList from './components/questions'
+import AppBar from './components/appbar'
 
 
-import Dialog from '@material-ui/core/Dialog';
-import AppBar from '@material-ui/core/AppBar';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+
+
+
+
 
 function App() {
   return (
-    <MuiThemeProvider>
-     <AppBar title="wqerrgf" />
     <div className="App">
-      <Places />
+      <AppBar />
       <QuestionList />
+      <Places />
     </div>
-      </MuiThemeProvider>
+      
   );
 }
 
